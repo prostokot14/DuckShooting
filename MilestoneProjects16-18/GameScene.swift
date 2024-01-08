35470442af4d8895da55a26c6994691adf72ba75
Anton Kashnikov <anton.kashnikoff@icloud.com>
2024-01-08 19:48:37 +0100

    Added score label

--- MilestoneProjects16-18/GameScene.swift
+++ MilestoneProjects16-18/GameScene.swift
@@ -8,11 +8,20 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    private var scoreLabel: SKLabelNode!
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "Background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.position = CGPoint(x: 8, y: 720)
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.fontSize = 48
+        addChild(scoreLabel)
     }
 }
